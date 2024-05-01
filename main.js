7780c2de802fdf027bd78238f568dc095039a2a5
Muhammad Naeem Warsi <161728783+MuhammadNaeemWarsi@users.noreply.github.com>
2024-05-02 01:15:55 +0500

    BMI calculator

---
+++ main.js
@@ -0,0 +1,4 @@
+var weightInKg = 75;
+var heightInMeter = 1.72;
+var BMI = (75 / Math.pow(1.72, 1.72));
+console.log("your BMI is ".concat(BMI));
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInKg=75  ;
+let heightInMeter=1.72 ;
+ let BMI=(75/1.72**1.72 )
+console.log(`your BMI is ${BMI}`)
